89cceaf0442d7eb19460c0e9cee225f5b6a08875
jbhamilton <brad@bradleyhamilton.com>
2016-01-24 13:36:05 -0500

    Full rewrite for new API changes that broke the old script. Also greatly extended functionality

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
 Used to download music files from SoundCloud.com even if the download button is not available.
 
+Based on the working directory you run the script from, mp3 tracks in the directory will be checked to prevent
+tracks from being downloaded twice.
+
+Uses ID3 tagging to set:
+    - Artist name
+    - Title
+    - Genre
+    - Year
+
 ### 6/8/2014
   - now using songdetails lib to write ID3 tag information about artist name, track name, genre, and year released.
+
+### 1/24/2016
+  - Complete rewrite of script to work with soundcloud API changes.
+  - Expanded functionality to allow for more types of downloades and pagination.
 
 ___
 
@@ -14,14 +27,30 @@ ___
 Just run the python script like:
 
 ```shell
-$ soundcloud-downloader.py USER CLIENT_ID
+$ soundcloud-downloader.py [type] [user] [[option],...] USER CLIENT_ID
 ```
-* `USER`: the username like `bassenectar` (http://soundcloud.com/bassnectar)
-* `CLIENT_ID`: the soundcloud Client ID (this script uses the soundcloud api)
+* `TYPE` : The type of resources to download:
+    - `favorites` - The favorited (liked) tracks.
+    - `tracks` - The uploaded tracks (created by the user).
+    - `playlist` - A playlist (or set).
+    - `track` - A single track.
+* `USER` : the username (permalink) like `brockberrigan` (http://soundcloud.com/brockberrigan)
+* `OPTION(s)` : Options dependant on the specified type:
+    - `favorites` options :
+        - `limit` : The maximum number of favorites to fetch and download, if not provided all favorites will be
+          fetched.
+    - `tracks` options :
+        - `limit` : The maximum number of tracks to fetch and download, if not provided all tracks will be
+          fetched.
+    - `playlist` options :
+        - `playlist name` - The name (permalink) of the playlist owned by the `user` to download.
+    - `track` options :
+        - `track name` - The name (permalink) of the track created by the `user` to download.
+* `CLIENT_ID` : the soundcloud Client ID (this script uses the soundcloud api)
 
 ### Get the client ID
 
-Register your app on [http://soundcloud.com/you/apps/new](http://soundcloud.com/you/apps/new) and takes the `Client ID`.
+Register your app on [http://soundcloud.com/you/apps/new](http://soundcloud.com/you/apps/new) and grab your app `Client ID`.
 
 ### Integrate into the console
 
@@ -29,21 +58,36 @@ Edit your `.bashrc` file and paste the following code :
 
 ```shell
 function soundcloud() {
-    user=$1
-    type=$2
-    /path/to/soundcloud-downloader.py ${user} ${type} CLIENT_ID
+    /path/to/soundcloud-downloader.py "$@" CLIENT_ID
 }
 ```
-And to download the all tracks at one time from Bassnectar user:
+
+### Example usage
+
+
+Getting favorites:
 ```shell
-$ soundcloud bassnectar
-# same action with the `type` argument
-$ soundcloud bassnectar tracks 
+$ soundcloud favorites wekilledtheradio
+# Limit to 30 most recent favorites
+$ soundcloud favorites wekilledtheradio 30
 ```
-And my favorites:
+
+Getting tracks:
 ```shell
-$ soundcloud oziks favorites
+$ soundcloud tracks brockberrigan 
+# Limit to 10 most recent tracks 
+$ soundcloud tracks brockberrigan 10
 ```
-___
+
+Getting a playlist (set):
+```shell
+$ soundcloud playlist wekilledtheradio shuffle-and-repeat-45min-plus-mix-tapes 
+```
+
+Getting a single track:
+```shell
+$ soundcloud track chillchap people
+```
+
 
 HAVE FUN :-)
--- soundcloud-downloader.py
+++ soundcloud-downloader.py
@@ -5,104 +5,278 @@ import glob
 import songdetails
 import json
 
-if len(sys.argv) <= 1:
-	exit("You need to enter a user to download from")
+class SC:
 
-def get_url(track):
-	# regular expression for the string we will search for in waveform-url tag
-	regexp = 'https://w1.sndcdn.com/(.*?)_m.png'
+    clientId                = ''
 
-	# find the song ID, if any
-	match = re.search(regexp, track['waveform_url'])
+    requestLimit            = 50
 
-	if match:
-		# create a new stream hyperlink with the song ID
-		url = "http://media.soundcloud.com/stream/%s" % match.group(1)
-	else:
-		print "No song ID found for the %s user. Exiting." % sys.argv[1]
-		sys.exit()
+    requestOffset           = 0
 
-	return url
+    apiRequestUrl           = "https://api.soundcloud.com"
 
-def get_already_downloaded():
-        return glob.glob('*')
+    apiRequestExtras        = "?client_id={0}&limit={1}&offset={2}"
+
+    apiDownloadEndPoint     = "https://api.soundcloud.com/i1/tracks/{0}/streams?client_id={1}"
+
+    alreadyDownloadedTracks = []
+
+    failedTracks            = []
+
+    skippedTrackCount       = 0
+
+    requestTypes            = ['track', 'tracks', 'favorites', 'playlist']
 
 
-def main():
-        failedTracks = []
-	print "Getting Information... "
 
-        #get the titles of the tracks we already have downloaded in the directory
-	alreadyDownloadedTracks = get_already_downloaded()
+    def __init__(self,clientId):
 
-	# retrieve the user of the songs to download
-	user = sys.argv[1]
+        if not clientId:
+            exit("You failed to supply your SoundCloud Client ID")
 
-	# retrieve type to download (tracks or favorites)
-	type = sys.argv[2]
+        self.clientId = clientId
 
-	if type != "tracks" and type != "favorites":
-		type = "tracks"
 
-	# retrieve the client_id from the final command-line argument
-	client_id = sys.argv[-1]
 
-	# retrieve the URL of the song to download from the final command-line argument
-	soundcloud_api = "https://api.soundcloud.com/users/%s/%s?client_id=%s&limit=9999&offset=0" % (user, type, client_id)
+    def run(self):
+
+        self.getAlreadyDownloadedTracks()
+
+        argCount = len(sys.argv)
+
+        if argCount < 2:
+            exit("You must specify a requestType: {0}".format(self.requestTypes));
+
+        requestType = sys.argv[1]
+
+        if requestType not in self.requestTypes:
+            exit("Request type {0} not supported!".format(requestType))
+
+        if argCount < 3:
+            exit("You must specify a user to download from!");
+
+        user = sys.argv[2]
+
+
+        if requestType == 'playlist':
+
+            if argCount < 4:
+                exit("You must specify a playlist name");
+
+            playlistName = sys.argv[3]
+
+            playlist = self.resolvePlaylist(user,playlistName)
+
+            if not playlist['id']:
+                exit("Unable to find playlist '{0}' by user '{1}'".format(playlistName,user))
+
+            self.downloadTracks(playlist['tracks'])
+
+        elif requestType == 'favorites' or requestType == 'tracks':
+
+            q = "/users/{0}/{1}".format(user,requestType)
+
+            limit = 'all'
+
+            if argCount == 5:
+                limit = int(sys.argv[3])
+
+            tracks = self.pageData(q,limit)
+
+            self.downloadTracks(tracks)
+
+        elif requestType == 'track':
+
+            if argCount < 4:
+                exit("You must specify a track to download!");
+
+            track = sys.argv[3]
+
+            track = self.resolveTrack(user,track)
+
+            if not track['id']:
+                exit("Could not find track '{0}' by user '{1}'".format(track,user))
+
+            q = "/tracks/{0}".format(track['id'])
+
+            track = self.apiRequest(self.buildApiEndPoint(q))
+
+            self.downloadTrack(track)
+
+        
+        # Reset the skipped track count
+        self.skippedTrackCount = 0
+
+
+
+    def pageData(self,q,limit):
+
+        allData     = []
+
+        if limit != 'all':
+            self.requestLimit = limit
+
+        while True:
+
+            data = self.apiRequest(self.buildApiEndPoint(q))
+
+            if len(data):
+                allData.extend(data)
+                self.requestOffset += self.requestLimit
+            else:
+                break
+
+            if limit != 'all' and self.requestOffset >= limit:
+                break;
+
+        
+        self.requestOffset = 0
+        self.requestLimit = 50
+
+        return allData
+
+
+
+    def resolveTrack(self,user,trackName):
+        return self.resolve("{0}/{1}".format(user,trackName))
+
+
+
+    def resolvePlaylist(self,user,playlistName):
+        return self.resolve("{0}/sets/{1}".format(user,playlistName))
+
+
+
+    def resolve(self,q):
+        return self.apiRequest(self.apiRequestUrl + "/resolve?url=http://soundcloud.com/{0}&client_id={1}".format(q,self.clientId))
+
+
+
+    def buildApiEndPoint(self,q):
+        return self.apiRequestUrl + q + self.apiRequestExtras.format(self.clientId,self.requestLimit,self.requestOffset)
+
+
+
+    def apiRequest(self,url):
 
 	try:
-		# open api URL for reading
-		data = urllib2.urlopen(soundcloud_api).read()
-		tracks = json.loads(data)
+
+            data = urllib2.urlopen(url).read()
+
+            return json.loads(data)
+
 	except ValueError:
-		# the user supplied URL is invalid or could not be retrieved
-		exit("Error: The user '%s' can not be retrieved" % user)
+            exit("Error: The user '%s' can not be retrieved" % user)
 
 
-	print "Ready to download the %s %s of the %s user... " % (len(tracks), type, user)
 
-        # download songs for each track for the user
+    def getAlreadyDownloadedTracks(self):
+
+        self.alreadyDownloadedTracks = glob.glob('*')
+
+
+
+    def getDownloadUrl(self,track):
+    
+    	if not track['id']:
+
+            print "No song ID found for the %s user. Exiting." % sys.argv[1]
+            sys.exit()
+
+        url = self.apiDownloadEndPoint.format(track['id'],self.clientId) 
+
+        try:
+
+            data = json.loads(urllib2.urlopen(url).read())
+
+            if data['http_mp3_128_url']:
+                return data['http_mp3_128_url']
+
+        except IOError:
+            print "IOError getting download url for track {0}".format(track['permalink'])
+	except ValueError:
+            print "ValueError getting download url for track {0}".format(track['permalink'])
+	except KeyError:
+            print "ValueError getting download url for track {0}".format(track['permalink'])
+
+
+        return False
+
+
+
+    def downloadTrack(self,track):
+
+
+        title       = track['title'].replace('.mp3','')
+        filename    = title + '.mp3'
+        artist      = track['user']['username']
+        genre       = track['genre']
+        year        = track['release_year']
+
+        try:
+
+            #if the song already exsists in the directory do nothing
+            if filename in self.alreadyDownloadedTracks:
+                print 'Song Already Downloaded : '+filename+'\n'
+                self.skippedTrackCount += 1
+                return
+
+            url = self.getDownloadUrl(track)
+
+            if not url:
+                self.failedTracks.append(title)
+                print "Unable to get download url for track '{0}', continueing to next track".format(title)
+                return
+
+            print "Downloading track '{0}'".format(title)
+
+            try:
+
+                urllib.urlretrieve(url, title)
+
+                # set the ID3 tagging information for the track
+                song = songdetails.scan(title)
+                if song is not None:
+                    song.artist = artist
+                    song.title = title
+                    song.genre = genre
+                    song.year = year
+                    song.save()
+
+                os.rename( title, filename )
+
+            except IOError as e:
+                self.failedTracks.append(title)
+                print 'Connection to SoundCloud Failed, unable to download:\n '+title+'\n continuing to next song'
+
+        except UnicodeEncodeError as e:
+            print 'Could not handle printing the unicode for the track\n'
+
+
+
+    def downloadTracks(self,tracks):
+
 	for track in tracks:
+	    self.downloadTrack(track)
 
-	        title = track['title'].replace('.mp3','')
-	        filename = title+'.mp3'
-	        artist = track['user']['username']
-	        url = get_url(track)
-	        genre = track['genre']
-	        year = track['release_year']
+	failCount    = len(self.failedTracks)
+	successCount = len(tracks) - failCount - self.skippedTrackCount
 
-                try:
-                    #if the song already exsists in the directory do nothing
-	            if filename in alreadyDownloadedTracks:
-                        print 'Song Already Downloaded : '+filename+'\n'
-                        continue
+        print "\n\n******************** RUN STATS ***********************\n"
+        print " {0} - Successfully downloaded".format(successCount)
+        print " {0} - Skipped (already downloaded)".format(self.skippedTrackCount)
+        print " {0} - Tracks failed to download : \n".format(failCount)
 
-		    print "Downloading File '%s'" % filename
-                    try:
-                        urllib.urlretrieve(url, title)
-                        # set the ID3 tagging information for the track
-                        song = songdetails.scan(title)
-                        if song is not None:
-                            song.artist = artist
-                            song.title = title
-                            song.genre = genre
-                            song.year = year
-                            song.save()
+        for track in self.failedTracks:
+            print "     " + track
 
-                        os.rename( title, filename )
+        print "\n******************************************************\n\n\n"
 
-                    except IOError as e:
-                        failedTracks.append(title)
-                        print 'Connection to SoundCloud Failed, unable to download:\n '+title+'\n continuing to next song'
-                except UnicodeEncodeError as e:
-                    print 'Could not handle printing the unicode for the track\n'
-
-
-	print "Download Complete"
-        if len(failedTracks)>0:
-            print 'Failed Tracks:\n'
-            for t in failedTracks:
-                print t+'\n'
 
 if __name__ == '__main__':
-	main()
+
+    clientId = sys.argv[-1]
+
+    soundcloud = SC(clientId)
+    soundcloud.run()
+
